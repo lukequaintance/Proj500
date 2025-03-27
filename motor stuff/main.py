ccb31a665b8162eae051745f65b7b5b55f63ba43
RhynJ <87622045+RhynJ@users.noreply.github.com>
2025-03-27 12:27:45 +0000

    added to main

---
+++ motor stuff/main.py
@@ -0,0 +1,48 @@
+import lgpio
+import time
+import board
+import busio
+from adafruit_ina219 import INA219
+import motorDriver
+
+#this inits the pins for all the stuff associated with the motor 
+motorDriver.setUpMotor()
+
+
+
+
+
+while True:
+    print("Motor Forward")
+    motorDriver.probeMove("forward")
+    motorDriver.testMove("forward")
+    time.sleep(5)
+    # Read values
+    bus_voltage = ina.bus_voltage      # Voltage in V
+    shunt_voltage = ina.shunt_voltage  # Voltage drop across shunt resistor in V
+    current = ina.current              # Current in mA
+    power = ina.power                  # Power in mW
+    # Print results
+    #print(f"Bus Voltage: {bus_voltage:.2f} V")
+    #print(f"Shunt Voltage: {shunt_voltage:.5f} V")
+    #print(f"Current: {current:.2f} mA")
+    #print(f"Power: {power:.2f} mW")
+    load_output = loadOnMotor(current)
+    print(f"Load: {load_output:.2f}")
+
+    print("Motor Backward")
+    probeMove("backward")
+    testMove("backward")
+    time.sleep(5)
+    # Read values
+    bus_voltage = ina.bus_voltage      # Voltage in V
+    shunt_voltage = ina.shunt_voltage  # Voltage drop across shunt resistor in V
+    current = ina.current              # Current in mA
+    power = ina.power                  # Power in mW
+    # Print results
+    #print(f"Bus Voltage: {bus_voltage:.2f} V")
+    #print(f"Shunt Voltage: {shunt_voltage:.5f} V")
+    #print(f"Current: {current:.2f} mA")
+    #print(f"Power: {power:.2f} mW")
+    #load_output = loadOnMotor(current)
+    #print(f"Load: {load_output:.2f}")
