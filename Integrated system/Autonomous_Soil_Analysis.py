a175466f2bca1657604872830fb6b7bdca78da55
SayeedRahman5 <sayeed.rahman@students.plymouth.ac.uk>
2025-04-23 15:01:37 +0100

    Removed key inputs

--- Integrated system/Autonomous_Soil_Analysis.py
+++ Integrated system/Autonomous_Soil_Analysis.py
@@ -8,9 +8,15 @@ import termios
 import tty
 from collections import deque
 import threading
+import serial
+import time
+import json
 
 # Initialize the motor
 motorDriver.setUpMotor()
+motorDriver.testMove("backward")
+motorDriver.probeMove("backward")
+
 
 # Rolling average setup
 ROLLING_WINDOW_SIZE = 20
@@ -32,17 +38,17 @@ print_lock = threading.Lock()
 stop_event = threading.Event()
 
 
-
-def get_key():
-    """Reads a single key press from the user without needing Enter."""
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(fd)
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+#DEBUGGING
+#def get_key():
+#    """Reads a single key press from the user without needing Enter."""
+#    fd = sys.stdin.fileno()
+#    old_settings = termios.tcgetattr(fd)
+#    try:
+#        tty.setraw(fd)
+#        ch = sys.stdin.read(1)
+#    finally:
+#        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+#    return ch
 
 def sample_current()-> float:
     
@@ -55,71 +61,59 @@ def sample_current()-> float:
 
 
 
-print("Use 'w' to move forward, 's' to move backward, and 'q' to quit.")
+#print("Use 'w' to move forward, 's' to move backward, and 'q' to quit.")
 
 # Main loop
 while True:
-    key = get_key()
-    
-    with print_lock:
-        if key == 'w':
+    motorDriver.testMove("forward")
+            
+    start_time = time.perf_counter() #start a timer for the stroke length
+    last_action_time = start_time # sample the timer 
+    current_time = time.perf_counter()
+    elapsed = current_time - start_time 
+            
+    print("testing for rocks")
+    time.sleep(2) # wait for the current to stabalise            
+
+    while elapsed < 35:
+        avg_current = sample_current()
+        last_action_time = start_time # sample the timer 
+        current_time = time.perf_counter()
+        elapsed = current_time - start_time 
+        print(avg_current)
+        print(elapsed)
+
+        if avg_current > current_when_rock:
+            motorDriver.testMove("backward")
+            print("there is a rock. moving and trying again")
+            time.sleep(40) # wait for 40 second in case the it was at full stroke 
+            # move buggy to new location 
             motorDriver.testMove("forward")
+            print("moving forwards")    
+            time.sleep(1)
+            #reset the timer back to 0
+            start_time = time.perf_counter()
+            last_action_time = start_time
+            elapsed = 0.0
+            rolling_current.clear() # clear the average so the spike does not interrupt the reading
             
-            start_time = time.perf_counter() #start a timer for the stroke length
-            last_action_time = start_time # sample the timer 
-            current_time = time.perf_counter()
-            elapsed = current_time - start_time 
-            
-            print("testing for rocks")
-            time.sleep(2) # wait for the current to stabalise            
-
-            while elapsed < 35:
-                avg_current = sample_current()
-                last_action_time = start_time # sample the timer 
-                current_time = time.perf_counter()
-                elapsed = current_time - start_time 
-                print(avg_current)
-                print(elapsed)
-
-                if avg_current > current_when_rock:
-                    motorDriver.testMove("backward")
-                    print("thre is a rock. moving and trying again")
-                    time.sleep(40) # wait for 40 second in case the it was at full stroke 
-                    # move buggy to new location 
-                    motorDriver.testMove("forward")
-                    print("moving forwards")    
-                    time.sleep(1)
-                    #reset the timer back to 0
-                    start_time = time.perf_counter()
-                    last_action_time = start_time
-                    elapsed = 0.0
-                    rolling_current.clear() # clear the average so the spike does not interrupt the reading
-            
-            print("this ground is soft enough, moving soil sensor for testing")
-            motorDriver.testMove("backward")
-            time.sleep(20)
-            # move RTU
-            print("probing senesor")
-            motorDriver.probeMove("forward")
-            time.sleep(30)
-            time.sleep(sensorTestTime)
-            #read sensor 
-            print("moving sensor probe backwards")
-            motorDriver.probeMove("backward")
-            time.sleep(30)
-            # continue with the code
+    print("this ground is soft enough, moving soil sensor for testing")
+    motorDriver.testMove("backward")
+    time.sleep(20)
+    # move RTU
+    print("probing senesor")
+    motorDriver.probeMove("forward")
+    time.sleep(30)
+    time.sleep(sensorTestTime)
+    #read sensor 
+    print("moving sensor probe backwards")
+    motorDriver.probeMove("backward")
+    motorDriver.testMove("backward")
+    time.sleep(30)
+    # continue with the code
 
             
 
 
 
-        elif key == 's':
-            motorDriver.probeMove("backward")
-            motorDriver.testMove("backward")
-            print("Moving backward...")
-    
-
-        elif key == 'q':
-            print("Exiting...")
-            running = False
-            break
+      
