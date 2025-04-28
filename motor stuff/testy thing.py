e8fdbd30d70b13ced3c1069627d87248e8ae6f09
lukequaintance <luke.quaintance@students.plymouth.ac.uk>
2025-04-28 19:08:10 +0100

    Update testy thing.py

--- motor stuff/testy thing.py
+++ motor stuff/testy thing.py
@@ -1,31 +1,68 @@
-from gpiozero import PWMOutputDevice, DigitalOutputDevice
-from time import sleep
+import RPi.GPIO as GPIO
+import time
 
-# Define GPIO pins
-RPWM_pin = 18
-LPWM_pin = 19
-REN_pin = 23  # optional, any free GPIO
-LEN_pin = 24  # optional, any free GPIO
+# Pin setup
+RPWM = 18
+LPWM = 23
+R_EN = 24
+L_EN = 25
 
-# Setup motor driver pins
-RPWM = PWMOutputDevice(RPWM_pin)
-LPWM = PWMOutputDevice(LPWM_pin)
-REN = DigitalOutputDevice(REN_pin)
-LEN = DigitalOutputDevice(LEN_pin)
+# GPIO mode
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-# Enable motor driver
-REN.on()
-LEN.on()
+# Setup pins
+GPIO.setup(RPWM, GPIO.OUT)
+GPIO.setup(LPWM, GPIO.OUT)
+GPIO.setup(R_EN, GPIO.OUT)
+GPIO.setup(L_EN, GPIO.OUT)
 
-while True:
-    # Motor forward
-    print("Motor forward")
-    RPWM.value = 0.7  # 70% speed
-    LPWM.value = 0
-    sleep(2)
+# PWM setup
+pwm_r = GPIO.PWM(RPWM, 1000)  # 1 kHz frequency
+pwm_l = GPIO.PWM(LPWM, 1000)
 
-    # Motor backward
-    print("Motor backward")
-    RPWM.value = 0
-    LPWM.value = 0.7  # 70% speed
-    sleep(2)
+# Start PWM with 0% duty cycle
+pwm_r.start(0)
+pwm_l.start(0)
+
+def motor_forward(speed):
+    GPIO.output(R_EN, GPIO.HIGH)
+    GPIO.output(L_EN, GPIO.HIGH)
+    pwm_r.ChangeDutyCycle(speed)  # speed 0-100%
+    pwm_l.ChangeDutyCycle(0)
+
+def motor_backward(speed):
+    GPIO.output(R_EN, GPIO.HIGH)
+    GPIO.output(L_EN, GPIO.HIGH)
+    pwm_r.ChangeDutyCycle(0)
+    pwm_l.ChangeDutyCycle(speed)
+
+def motor_stop():
+    GPIO.output(R_EN, GPIO.LOW)
+    GPIO.output(L_EN, GPIO.LOW)
+    pwm_r.ChangeDutyCycle(0)
+    pwm_l.ChangeDutyCycle(0)
+
+try:
+    while True:
+        print("Forward")
+        motor_forward(80)  # 80% speed
+        time.sleep(2)
+
+        print("Stop")
+        motor_stop()
+        time.sleep(1)
+
+        print("Backward")
+        motor_backward(80)  # 80% speed
+        time.sleep(2)
+
+        print("Stop")
+        motor_stop()
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Cleaning up GPIO")
+    pwm_r.stop()
+    pwm_l.stop()
+    GPIO.cleanup()
