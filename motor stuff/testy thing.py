7ef19b8e11b6c99e7af5676f8ea64194b25dfb27
lukequaintance <luke.quaintance@students.plymouth.ac.uk>
2025-04-28 19:23:27 +0100

    Update testy thing.py

--- motor stuff/testy thing.py
+++ motor stuff/testy thing.py
@@ -1,68 +1,42 @@
 import RPi.GPIO as GPIO
 import time
 
-# Pin setup
-RPWM = 18
-LPWM = 23
-R_EN = 24
-L_EN = 25
+# Define pins
+RPWM = 18  # GPIO18 (Pin 12)
+LPWM = 13  # GPIO13 (Pin 33)
+REN = 23   # GPIO23 (Pin 16)
+LEN = 24   # GPIO24 (Pin 18)
 
-# GPIO mode
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-# Setup pins
 GPIO.setup(RPWM, GPIO.OUT)
 GPIO.setup(LPWM, GPIO.OUT)
-GPIO.setup(R_EN, GPIO.OUT)
-GPIO.setup(L_EN, GPIO.OUT)
+GPIO.setup(REN, GPIO.OUT)
+GPIO.setup(LEN, GPIO.OUT)
 
-# PWM setup
-pwm_r = GPIO.PWM(RPWM, 1000)  # 1 kHz frequency
+# Create PWM objects
+pwm_r = GPIO.PWM(RPWM, 1000)  # 1kHz frequency
 pwm_l = GPIO.PWM(LPWM, 1000)
 
-# Start PWM with 0% duty cycle
-pwm_r.start(0)
-pwm_l.start(0)
+# Enable the driver
+GPIO.output(REN, GPIO.HIGH)
+GPIO.output(LEN, GPIO.HIGH)
 
-def motor_forward(speed):
-    GPIO.output(R_EN, GPIO.HIGH)
-    GPIO.output(L_EN, GPIO.HIGH)
-    pwm_r.ChangeDutyCycle(speed)  # speed 0-100%
-    pwm_l.ChangeDutyCycle(0)
+# Test: Motor Forward
+pwm_l.start(0)   # LPWM 0%
+pwm_r.start(50)  # RPWM 50%
 
-def motor_backward(speed):
-    GPIO.output(R_EN, GPIO.HIGH)
-    GPIO.output(L_EN, GPIO.HIGH)
-    pwm_r.ChangeDutyCycle(0)
-    pwm_l.ChangeDutyCycle(speed)
+time.sleep(2)
 
-def motor_stop():
-    GPIO.output(R_EN, GPIO.LOW)
-    GPIO.output(L_EN, GPIO.LOW)
-    pwm_r.ChangeDutyCycle(0)
-    pwm_l.ChangeDutyCycle(0)
+# Test: Motor Reverse
+pwm_r.ChangeDutyCycle(0)
+pwm_l.ChangeDutyCycle(50)
 
-try:
-    while True:
-        print("Forward")
-        motor_forward(80)  # 80% speed
-        time.sleep(2)
+time.sleep(2)
 
-        print("Stop")
-        motor_stop()
-        time.sleep(1)
+# Stop motor
+pwm_r.ChangeDutyCycle(0)
+pwm_l.ChangeDutyCycle(0)
 
-        print("Backward")
-        motor_backward(80)  # 80% speed
-        time.sleep(2)
-
-        print("Stop")
-        motor_stop()
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    print("Cleaning up GPIO")
-    pwm_r.stop()
-    pwm_l.stop()
-    GPIO.cleanup()
+pwm_r.stop()
+pwm_l.stop()
+GPIO.cleanup()
